75e369ccb7eff5fcee90f06823c5d55bbdd45501
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-08-31 09:29:58 +0200

    first commit

---
+++ snack1.php
@@ -0,0 +1,33 @@
+
+<?php
+$mtach_bascket_array = [
+    ["Home" => "Caserta ", "Away" => "Parma", "HomeScore" => "50", "AwayScore" => "60"],
+    ["Home" => "Bologna", "Away" => "Verona", "HomeScore" => "48", "AwayScore" => "65"],
+    ["Home" => "Pescara", "Away" => "Trento", "HomeScore" => "40", "AwayScore" => "90"],
+    ["Home" => "Sassari", "Away" => "Cremona", "HomeScore" => "80", "AwayScore" => "60"],
+    ["Home" => "Varese", "Away" => "Trieste", "HomeScore" => "60", "AwayScore" => "20"],
+    ["Home" => "salerno", "Away" => "Foggia", "HomeScore" => "80", "AwayScore" => "80"],
+    ["Home" => "Milano", "Away" => "Bari", "HomeScore" => "40", "AwayScore" => "56"],
+    ["Home" => "Cagliari", "Away" => "Roma", "HomeScore" => "72", "AwayScore" => "65"],
+]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+
+</head>
+<body>
+    <h1>Risultati Lega A</h1>
+    <h2>Giornata 29</h2>
+    <ul>
+        <?php for ($i = 0; $i < count($mtach_bascket_array); $i++): ?>
+        <li><?=$mtach_bascket_array[$i]['Home']?> - <?=$mtach_bascket_array[$i]['Away']?> : <strong><?=$mtach_bascket_array[$i]['HomeScore']?> | <?=$mtach_bascket_array[$i]['AwayScore']?></strong></li>
+        <?php endfor?>
+    </ul>
+</body>
+</html>
